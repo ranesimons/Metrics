ed431ea65b899b61d41b8d2d05e1ef16f70528e4
Rane Simons <ranesimons@gmail.com>
2025-09-03 21:39:04 -0500

    feat: added a public folder for static pages

--- api/index.js
+++ api/index.js
@@ -1,8 +1,13 @@
 const serverless = require('serverless-http');
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.get('/', (req, res) => {
+// Serve files from the public folder
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/api/hello', (req, res) => {
   res.send('Hello from Express on Vercel!');
 });
 
